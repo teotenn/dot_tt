3f83b50fe240ec36fcdb4100dd786b218c056fc5
Manuel Teodoro <teoten@gmail.com>
2023-04-03 12:29:04 +0200

    Tint2 configured; changes to use with Guix

--- bash/scripts/autostart.sh
+++ bash/scripts/autostart.sh
@@ -1,22 +1,9 @@
-if [ -f /usr/bin/thunar ]; then
 thunar --daemon &
-fi
-
-if [ -f /usr/bin/compton ]; then
-    compton &
-fi
-
-if [ -f /usr/bin/emacs ]; then
+compton &
 emacs --daemon &
-fi
 
 sleep 2
-if [ -f /home/teoten/Code/dot_tt/bash/scripts/sirius_xrandr.sh ]; then
-    /bin/bash /home/teoten/Code/dot_tt/bash/scripts/sirius_xrandr.sh
-fi
+/bin/bash /home/teoten/Code/dot_tt/bash/scripts/sirius_xrandr.sh
 
-if [ -f /usr/bin/conky ]; then
-    conky -q -c ~/Code/dot_tt/conky/blue-white.conkyrc & conky -q -c ~/Code/dot_tt/conky/clock.conkyrc &
-elif [ -f ~/.guix-profile/bin/conky ]; then
-    ~/.guix-profile/bin/conky -q -c ~/Code/dot_tt/conky/blue-white.conkyrc & ~/.guix-profile/bin/conky -q -c ~/Code/dot_tt/conky/clock.conkyrc &
-fi
+conky -q -c ~/Code/dot_tt/conky/blue-white.conkyrc & conky -q -c ~/Code/dot_tt/conky/clock.conkyrc &
+tint2 -c ~/Code/dot_tt/tint2/minima/minima.tint2rc &
