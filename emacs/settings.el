14feee03693459e7e3a2273e1829427a433c4914
teodorm3 <manuel.teodoro_tenango@contractors.roche.com>
2023-08-24 08:54:11 +0200

    Minor changes for win

--- emacs/settings.el
+++ emacs/settings.el
@@ -143,20 +143,20 @@
        '(rainbow-delimiters-depth-8-face ((t (:inherit rainbow-delimiters-base-face :foreground "bisque"))))))
 
     ;; yasnippet
-    (use-package yasnippet
-      :init
-      (setq yas-snippet-dirs
-	    '("~/.emacs.d/snippets"
-	      "~/Code/dot_tt/emacs/snippets"
-	      ))
-      :config
-      (yas-global-mode 1))
+(use-package yasnippet
+  :init
+  (setq yas-snippet-dirs
+	'("~/.emacs.d/snippets"
+	  "~/Code/dot_tt/emacs/snippets"
+	  ))
+  :config
+  (yas-global-mode 1))
 
 (use-package neotree)
 (use-package htmlize)
 
-  (use-package all-the-icons
-    :if (display-graphic-p))
+(use-package all-the-icons
+  :if (display-graphic-p))
 
 (use-package imenu-list
   :bind (("C-c m" . imenu-list-smart-toggle))
@@ -186,7 +186,7 @@
   :if (eq system-type 'windows-nt)
   :init
   (setq ess-use-flymake nil)
-  (setq inferior-ess-r-program "C:/Users/teodorm3/Bin/R-4.2.1/bin/R.exe"))
+  (setq inferior-ess-r-program "C:/Users/teodorm3/Bin/R-4.1.2/bin/R.exe"))
 
 (use-package ess
   :init
