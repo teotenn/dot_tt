1bbc7adb858e7bdbf7afb1cc7c9c709d0d8dbc69
Manuel Teodoro <teoten@gmail.com>
2023-10-14 07:44:19 +0200

    Basic config for awesome keeping personal keybins

--- bash/scripts/autostart.sh
+++ bash/scripts/autostart.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-if [ -f /usr/bin/thunar ] || [ -f ~/.guix-profile/bin/thunar ]; then
-    thunar --daemon &
-fi
-
+## Compton
 if [ -f /usr/bin/compton ] || [ -f ~/.guix-profile/bin/compton ]; then
     compton &
 fi
 
-if [ -f /usr/bin/emacs ] || [ -f ~/.guix-profile/bin/emacs ]; then
-    emacs --daemon &
+## Thunar
+if [ -f /usr/bin/thunar ] || [ -f ~/.guix-profile/bin/thunar ]; then
+    thunar --daemon &
 fi
 
+## xrandr
 sleep 2
 if [ -f /home/teoten/Code/dot_tt/bash/scripts/sirius_xrandr.sh ]; then
     bash /home/teoten/Code/dot_tt/bash/scripts/sirius_xrandr.sh
 fi
 
+## cony
 if [ -f /usr/bin/conky ] || [ -f ~/.guix-profile/bin/conky ]; then
     conky -q -c ~/Code/dot_tt/conky/blue-white.conkyrc &
 fi
 
+## Tint2
 if [ -f /usr/bin/tint2 ] || [ -f ~/.guix-profile/bin/tint2 ]; then
     tint2 -c /home/teoten/Code/dot_tt/tint2/minima/minima.tint2rc &
+fi
+
+## Emacs at the end
+if [ -f /usr/bin/emacs ] || [ -f ~/.guix-profile/bin/emacs ]; then
+    emacs --daemon &
 fi
